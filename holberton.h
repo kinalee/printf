71205bfc5b66b990503d7e13483b7cbf974dc497
LinuxHP <dantonallen@gmail.com>
2016-11-22 21:09:15 -0800

    removed addl prototypes

--- holberton.h
+++ holberton.h
@@ -10,10 +10,6 @@ int _strlen(char *str);
 int _numlen(long number);
 int _atoi(char *str);
 int print_int(long long int number);
-int print_octal(unsigned int number);
-void hex_handler(int copy, int lowercase);
-int print_small_hex(unsigned int number);
-int print_big_hex(unsigned int number);
 char *_itoa(long long int number, char *stringnum);
 
 /**
